f421d51b2312002f2553de3fb8d45fe60dbfc677
feiyuz <zyg3933@126.com>
2025-06-18 19:44:15 +0800

    第一次提交：我的计算器程序

---
+++ calculate.py
@@ -0,0 +1,8 @@
+print("✨ 超级计算器 ✨")
+num1 = float(input("第一个数字: "))
+num2 = float(input("第二个数字: "))
+
+print(f"{num1} + {num2} = {num1+num2}")
+print(f"{num1} - {num2} = {num1-num2}")
+print(f"{num1} × {num2} = {num1*num2}")
+print(f"{num1} ÷ {num2} = {num1/num2}")
